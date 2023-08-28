6c184e6d206b085f7dac10e6e3c164bb0d65180e
coder7475 <robiulhossain7475@gmail.com>
2023-08-28 16:21:52 +0600

    feat: show all button is shown if there are more results && add html for loading spinner

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,18 @@ function displayPhone(data) {
     // console.log(data);
     const fullData = data;
     data = data.slice(0, 12);
+    // get the container
     const container = document.getElementById('phone-container');
+    // clear the field
     container.innerHTML = "";
+    // show all button is visible if search result is more than 12
+    if (fullData.length > 12) {
+        document.getElementById('btn-sa').classList.remove('hidden');
+    }
+    else {
+        document.getElementById('btn-sa').classList.add('hidden');
+    }
+    // display cards
     data.forEach(element => {
         // console.log(element);
         const card = document.createElement('div');
@@ -20,7 +30,7 @@ function displayPhone(data) {
 
         card.innerHTML = `
         <figure><img src="${element.image}" alt="Phones" /></figure>
-        <div class="flex flex-col items-center p-6">
+        <div class="card-body flex flex-col items-center p-6">
           <h2 class="card-title">${element.phone_name}</h2>
           <p class="text-center mt-5">There are many variations of passages of available, but the majority have suffered</p>
           <h3 class="font-bold mt-2">$999</h3>
